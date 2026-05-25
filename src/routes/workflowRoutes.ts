888e1301720570cac9b4f923692dd4dd79d88ec5
ik059 <ibrahimmunna059@outlook.de>
2026-05-25 22:23:21 +0200

    task 6 completed - endpoint for retrieving workflow status

--- src/routes/workflowRoutes.ts
+++ src/routes/workflowRoutes.ts
@@ -3,6 +3,8 @@ import { AppDataSource } from '../data-source';
 import { Workflow } from '../models/Workflow';
 import { Task } from '../models/Task';
 import { TaskStatus } from '../workers/taskRunner'
+import { WorkflowStatus } from '../workflows/WorkflowFactory';
+import { stat } from 'fs';
 
 const router = Router();
 const workflowRepository = AppDataSource.getRepository(Workflow);
@@ -20,6 +22,7 @@ router.get('/:id/status', async (req, res): Promise<void> => {
 
         if(!existWorkflow){
             res.status(404).json({message: "Workflow not found"})
+            return
         }
         else{ 
             const completedTasks = existWorkflow.tasks.filter(t => t.status === TaskStatus.Completed).length;
@@ -31,12 +34,44 @@ router.get('/:id/status', async (req, res): Promise<void> => {
                 completedTasks: completedTasks,
                 totalTasks: totalTasks
             }
-
             res.status(200).send(response)
         }
     }
     catch(error){
         console.error("Error in fetching workflow status: ", error)
+        res.status(500).json({message: "Internal server error"})
+    }
+})
+
+router.get('/:id/results', async (req, res): Promise<void> =>{
+    const { id } = req.params
+
+    try{
+        const existWorkflow = await workflowRepository.findOne({
+            where:{
+                workflowId: id
+            }
+        })
+
+        if(!existWorkflow){
+            res.status(404).json({message: "Workflow not found"})
+            return
+        }
+        else if(existWorkflow.status !== WorkflowStatus.Completed){
+            res.status(400).json({message:"Workflow is not completed yet"})
+            return
+        }
+        else{
+            const response = {
+                "workflowId": existWorkflow.workflowId,
+                "status": existWorkflow.status,
+                "finalResult": existWorkflow.finalResult ? JSON.parse(existWorkflow.finalResult) : null
+            }
+            res.status(200).send(response)
+        }
+    }
+    catch(error){
+        console.log("Error in fetching results: ", error)
         res.status(500).json({message: "Internal server error"})
     }
 })
